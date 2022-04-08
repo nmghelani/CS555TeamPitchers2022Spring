6823113e251dbc188eda7d08ca960009236b4076
vdesai18 <vdesai18@stevens.edu>
2022-04-07 22:05:35 -0400

    Added welcome popup

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,8 @@
 (function ($) {
   $(this).css("cursor", "url(/public/images/hammer.png), auto");
+  Swal.fire({
+    title : "Are you frustrated",
+    text : "We have got everything to get rid of that fustration.",
+    confirmButtonText : "Let's go",
+  })
 })(jQuery);
