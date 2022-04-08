97dde637ec6fb6e8bec0d83753e4e885a9012c4e
rachna-eng <rshah4@stevens.edu>
2022-04-08 01:45:32 -0400

    Showing message on exit

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,23 @@
 (function ($) {
   $(this).css("cursor", "url(/public/images/hammer.png), auto");
   Swal.fire({
-    title : "Are you frustrated",
-    text : "We have got everything to get rid of that fustration.",
-    confirmButtonText : "Let's go",
-  })
+    title: "Are you frustrated",
+    text: "We have got everything to get rid of that fustration.",
+    confirmButtonText: "Let's go",
+  });
 })(jQuery);
+
+window.onbeforeunload = (event) => {
+  if (
+    !window.confirm(
+      "Getting back to work? I hope we helped you relieving your stress"
+    )
+  ) {
+    const e = event || window.event;
+    e.preventDefault();
+    if (e) {
+      e.returnValue = "";
+    }
+    return "";
+  }
+};
